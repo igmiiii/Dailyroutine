fc9db690159dcc5763bc54788968e89a86321973
igmiiii <sinker591@naver.com>
2025-01-03 09:27:14 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> arr) {
+    double answer = 0;
+    double sum = 0;
+    
+    for(int num : arr){
+        sum += num;
+        }
+    answer = sum / arr.size();
+    
+    return answer;
+}
