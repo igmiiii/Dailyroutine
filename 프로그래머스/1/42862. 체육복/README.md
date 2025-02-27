df0e9434f18296093ba18e84c2a64a6da3b10b17
igmiiii <sinker591@naver.com>
2025-02-27 21:57:23 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 4.13 MB, 시간: 0.02 ms
+메모리: 4.17 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 21:50:13
+2025년 02월 27일 21:57:21
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,45 +1,36 @@
+#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    // 1. 중복된 학생 처리: reserve에 있지만 lost에 있는 학생은 제외
+    int answer = 0;
+    
     sort(lost.begin(), lost.end());
     sort(reserve.begin(), reserve.end());
     
-    vector<int> actual_lost;
-    vector<int> actual_reserve;
-    
-    // 중복 처리 (여벌 체육복을 가진 학생은 lost에서 제외)
-    for (int i = 0; i < lost.size(); ++i) {
-        if (find(reserve.begin(), reserve.end(), lost[i]) != reserve.end()) {
-            // 여벌 체육복을 가져온 학생이 도난당했을 경우, 그 학생은 lost 배열에서 제외
-            reserve.erase(find(reserve.begin(), reserve.end(), lost[i]));
-        } else {
-            actual_lost.push_back(lost[i]);
-        }
-    }
-    
-    actual_reserve = reserve; // 실제 여벌 체육복을 가진 학생 목록
-
-    // 2. 여벌 체육복을 빌려줄 수 있는 학생 처리
-    for (int i = 0; i < actual_reserve.size(); ++i) {
-        int student = actual_reserve[i];
-        
-        // 앞 번호 학생에게 체육복을 빌려줄 수 있는지 확인
-        auto it = find(actual_lost.begin(), actual_lost.end(), student - 1);
-        if (it != actual_lost.end()) {
-            actual_lost.erase(it); // 체육복을 빌려준 학생은 리스트에서 제거
-        } else {
-            // 뒤 번호 학생에게 체육복을 빌려줄 수 있는지 확인
-            it = find(actual_lost.begin(), actual_lost.end(), student + 1);
-            if (it != actual_lost.end()) {
-                actual_lost.erase(it); // 체육복을 빌려준 학생은 리스트에서 제거
+   for (int i = 0; i < lost.size(); ++i) {
+        for (int j = 0; j < reserve.size(); ++j) {
+            if (lost[i] == reserve[j]) {
+                lost.erase(lost.begin() + i);  // 도난당한 학생에서 제외
+                reserve.erase(reserve.begin() + j);  // 여벌 체육복을 가진 학생에서 제외
+                i--;  // lost 배열에서 해당 인덱스를 제거했으므로 i를 하나 감소
+                break;  // 한 명만 처리하면 되므로 그만두기
             }
         }
     }
 
-    // 3. 체육수업을 들을 수 있는 학생의 수는 총 학생 수에서 아직 체육복이 없는 학생의 수를 뺀 값
-    return n - actual_lost.size();
+    // 체육복을 빌려주는 과정
+    for (int i = 0; i < reserve.size(); ++i) {
+        int student = reserve[i];
+        if (find(lost.begin(), lost.end(), student - 1) != lost.end()) {  // 앞번호 학생에게 빌려주기
+            lost.erase(find(lost.begin(), lost.end(), student - 1));
+        } else if (find(lost.begin(), lost.end(), student + 1) != lost.end()) {  // 뒷번호 학생에게 빌려주기
+            lost.erase(find(lost.begin(), lost.end(), student + 1));
+        }
+    }
+
+    // 체육수업을 들을 수 있는 학생의 수는 총 학생 수에서 아직 체육복이 없는 학생의 수를 뺀 값
+    return n - lost.size();
 }
