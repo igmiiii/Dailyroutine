c21c4e66b6c3a4948368cb0eafea518ff9d40a75
igmiiii <sinker591@naver.com>
2025-02-28 21:56:36 +0900

    [level 1] Title: 둘만의 암호, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.cpp
@@ -1,37 +1,27 @@
 #include <string>
-#include <vector>
+#include <unordered_set>
 
 using namespace std;
 
 string solution(string s, string skip, int index) {
-    string answer = "";
-    
-    
-    
-    for (char c : s) {
-        char new_char = c;  // 현재 문자 c를 새로운 문자로 처리할 변수
+    string result = "";
+    unordered_set<char> skipSet; // skip에 포함된 알파벳들을 저장할 set
 
-        // 문자를 index만큼 이동시키기
-        for (int i = 0; i < index; ++i) {
-            new_char++;  // 문자 하나씩 이동
-
-            // 'z'를 넘어가면 'a'로 돌아가기
-            if (new_char > 'z') {
-                new_char = 'a';
-            }
-
-            // 만약 'skip'에 포함된 문자라면, 건너뛰고 계속 이동
-            while (skip.find(new_char) != string::npos) {
-                new_char++;  // skip에 포함되었으면 다음 문자로 이동
-                if (new_char > 'z') {
-                    new_char = 'a';  // 다시 'a'로 돌아가기
-                }
-            }
-        }
-
-        // 새로 계산된 문자를 결과에 추가
-        answer += new_char;
+    // skip 문자열에 포함된 알파벳을 set에 넣기
+    for (char c : skip) {
+        skipSet.insert(c);
     }
 
-    return answer;
+    for (char c : s) {
+        int count = 0;
+        while (count < index) {
+            c = (c == 'z') ? 'a' : c + 1; // z를 넘어가면 a로 돌아감
+            if (skipSet.find(c) == skipSet.end()) { // skip에 포함되지 않으면
+                count++;
+            }
+        }
+        result += c;
+    }
+
+    return result;
 }
