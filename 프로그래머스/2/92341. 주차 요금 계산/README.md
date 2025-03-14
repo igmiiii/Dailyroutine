b6fd53fa6e4b579e1e452c91d577dd01e2497199
igmiiii <sinker591@naver.com>
2025-03-14 11:44:21 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.46 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#qna) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 1.19 ms
+메모리: 4.21 MB, 시간: 1.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 14일 06:12:54
+2025년 03월 14일 11:44:20
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.cpp
@@ -1,62 +1,60 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <cmath>
+#include <queue>
 
 using namespace std;
 
 vector<int> solution(vector<int> fees, vector<string> records) {
     vector<int> answer;
     
-    int DefaultTime = fees[0];  // 기본 시간 (분)
-    int DefaultFee = fees[1];   // 기본 요금 (원)
-    int UnitMinute = fees[2];   // 단위 시간 (분)
-    int UnitFee = fees[3];      // 단위 요금 (원)
+    map<string, queue<int>> Cars;
     
-    map<string, int> parking;  // 각 차량별 주차 시간 저장
-    map<string, int> entryTime;  // 각 차량의 입차 시간 저장
+    int DefaultTime = fees[0];
+    int DefaultFee = fees[1];
+    int UnitMinute = fees[2];
+    int UnitFee = fees[3];
     
-    // 입/출차 기록 처리
-    for (string record : records) {
-        string timeStr = record.substr(0, 5);  // "HH:MM"
-        string carNumber = record.substr(6, 4);  // 차량 번호
-        string action = record.substr(11, 2);  // "IN" 또는 "OUT"
-        
-        int hour = stoi(timeStr.substr(0, 2));
-        int minute = stoi(timeStr.substr(3, 2));
-        int time = hour * 60 + minute;  // "HH:MM"을 분으로 변환
-        
-        if (action == "IN") {
-            // 차량 입차 시간 기록
-            entryTime[carNumber] = time;
-        } else {  // OUT
-            // 차량 출차 시간 계산
-            int parkedTime = time - entryTime[carNumber];
-            parking[carNumber] += parkedTime;
-            entryTime.erase(carNumber);  // 출차 후 입차 기록 삭제
-        }
+    for(string s : records)
+    {
+        int Time = 0;
+        Time += stoi(s.substr(0, 2))*60;
+        Time += stoi(s.substr(3, 2));
+        Cars[s.substr(6, 4)].push(Time);
     }
     
-    // 출차 기록이 없는 차량은 23:59에 출차된 것으로 간주
-    for (auto& entry : entryTime) {
-        int parkedTime = (23 * 60 + 59) - entry.second;  // 23:59에서 출차 시간 계산
-        parking[entry.first] += parkedTime;
-    }
-    
-    // 차량 번호 순으로 주차 요금을 계산
-    for (auto& entry : parking) {
-        string carNumber = entry.first;
-        int parkedTime = entry.second;
-        
-        int fee = DefaultFee;
-        
-        if (parkedTime > DefaultTime) {
-            int extraTime = parkedTime - DefaultTime;
-            int extraFee = (int)ceil((double)extraTime / UnitMinute) * UnitFee;
-            fee += extraFee;
+    for(auto& pair : Cars)
+    {
+        int Sum = 0;
+        while(!pair.second.empty())
+        {
+            int Temp = pair.second.front();
+            pair.second.pop();
+            if(!pair.second.empty())
+            {
+                Sum += pair.second.front() - Temp;
+                pair.second.pop();
+            }
+            else
+            {
+                Sum += 1439 - Temp;
+            }
         }
-        
-        answer.push_back(fee);
+        if(Sum<= DefaultTime)
+        {
+            answer.push_back(DefaultFee);
+        }
+        else
+        {
+            if((Sum - DefaultTime)%UnitMinute== 0)
+            {
+                answer.push_back(DefaultFee + ((Sum - DefaultTime)/UnitMinute) * UnitFee);
+            }
+            else
+            {
+                answer.push_back(DefaultFee + ((Sum - DefaultTime)/UnitMinute+1) * UnitFee);
+            }
+        }
     }
     
     return answer;
